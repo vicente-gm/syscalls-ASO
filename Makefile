edee4ec97397c8a4cd6de9f78da02b8fa4e6c8cf
Vicente <vicente.gnzmls@gmail.com>
2024-10-11 10:58:00 +0200

    Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+exec_lines: exec_lines.c
+	gcc -ggdb3 -Wall -Werror -Wno-unused -std=c11 exec_lines.c -o exec_lines
---
+++ exec_lines.c
@@ -0,0 +1,6 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!\n");
+}
