a74a537ef71c1d9a1678189b4d2d03cc59743207
Vicente <vicente.gnzmls@gmail.com>
2024-10-15 13:48:00 +0200

    Prueba

--- exec_lines.c
+++ exec_lines.c
@@ -3,6 +3,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <assert.h>
 #include <fcntl.h>
@@ -21,22 +22,12 @@ void print_help(char* nombre_programa)
 	fprintf(stderr, "Uso: %s [-b BUF_SIZE] [-l MAX_LINE_SIZE]\nLee de la entrada estándar una secuencia de líneas conteniendo órdenes\npara ser ejecutadas y lanza los procesosnecesarios para ejecutar cada línea, esperando a su terminacion para ejecutar la siguiente.\n-b BUF_SIZE\tTamaño del buffer de entrada 1<=BUF_SIZE<=8192\n-l MAX_LINE_SIZE\tTamaño máximo de línea 16<=MAX_LINE_SIZE<=1024\n", nombre_programa);
 }
 
-void read_line(char *buffer, char *comBuffer, int buf_size, int max_line_size)
-{
-    ssize_t read_words;
-
-    while(read_words = read(STDIN_FILENO, buffer, buf_size))
-    {
-
-    }
-}
-
 int main(int argc, char *argv[]) 
 {
     int opt;
     int buf_size = DEFAULT_BUF_SIZE;
-	int line_size = DEFAULT_LINE_SIZE;
-	char *buffer;
+    int line_size = DEFAULT_LINE_SIZE;
+    char *buffer;
     char *bufferComando;
     int line;
 
@@ -64,24 +55,25 @@ int main(int argc, char *argv[])
 
     if(buf_size == -1)
     {
-		fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
-		print_help(argv[0]);
-		exit(EXIT_FAILURE);
-	}
+	fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
+	print_help(argv[0]);
+	exit(EXIT_FAILURE);
+    }
 
-	if(line_size == -1)
+    if(line_size == -1)
     {
-		fprintf(stderr, "%s: option requires an argument -- 'l'\n", argv[0]);
-		print_help(argv[0]);
-		exit(EXIT_FAILURE);
-	}
+	fprintf(stderr, "%s: option requires an argument -- 'l'\n", argv[0]);
+	print_help(argv[0]);
+	exit(EXIT_FAILURE);
+    }
 
-    if ((buf_size < MIN_BUF_SIZE) || (buf_size > MAX_BUF_SIZE)){
-		fprintf(stderr, "Error: El tamaño de buffer tiene que estar entre 1 y 8192.\n");
-		fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
-		print_help(argv[0]);
-		exit(EXIT_FAILURE);
-	}
+    if ((buf_size < MIN_BUF_SIZE) || (buf_size > MAX_BUF_SIZE)) 
+    {
+	fprintf(stderr, "Error: El tamaño de buffer tiene que estar entre 1 y 8192.\n");
+	fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
+	print_help(argv[0]);
+	exit(EXIT_FAILURE);
+    }
 
 	if ((line_size < MIN_LINE_SIZE) || (line_size > MAX_LINE_SIZE)){
 		fprintf(stderr, "Error: El tamaño máximo de linea tiene que estar entre 16 y 1024.\n");
@@ -91,7 +83,7 @@ int main(int argc, char *argv[])
 	}
 
      /* Reserva memoria dinámica para buffer de lectura */
-    if ((buffer = (char *) malloc(BUF_SIZE * sizeof(char))) == NULL)
+    if ((buffer = (char *) malloc(buf_size * sizeof(char))) == NULL)
     {
         perror("malloc()");
         exit(EXIT_FAILURE);
@@ -103,7 +95,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("%d", buf_size);
-    printf("%d", line_size);
+    printf("Size of buffer: %d\n", buf_size);
+    printf("Max. size of line: %d\n", line_size);
 
 }
