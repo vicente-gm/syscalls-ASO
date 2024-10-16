1caf73021c317516941c8aa5677cbdcec579be75
Vicente <vicente.gnzmls@gmail.com>
2024-10-16 23:20:50 +0200

    Struct line_t

--- exec_lines.c
+++ exec_lines.c
@@ -15,20 +15,22 @@
 #define DEFAULT_LINE_SIZE 32
 #define MIN_LINE_SIZE 16
 
+#define SEPARATOR " "
+#define CONTROL_CHAR "\n"
 
 
-void print_help(char* nombre_programa)
-{
-	fprintf(stderr, "Uso: %s [-b BUF_SIZE] [-l MAX_LINE_SIZE]\nLee de la entrada estándar una secuencia de líneas conteniendo órdenes\npara ser ejecutadas y lanza los procesosnecesarios para ejecutar cada línea, esperando a su terminacion para ejecutar la siguiente.\n-b BUF_SIZE\tTamaño del buffer de entrada 1<=BUF_SIZE<=8192\n-l MAX_LINE_SIZE\tTamaño máximo de línea 16<=MAX_LINE_SIZE<=1024\n", nombre_programa);
-}
+
+void print_help(char* nombre_programa);
+
+typedef struct line *line_t;
+void execute_line(line_t line);
+
 
 int main(int argc, char *argv[]) 
 {
     int opt;
     int buf_size = DEFAULT_BUF_SIZE;
     int line_size = DEFAULT_LINE_SIZE;
-    char *buffer;
-    char *bufferComando;
     int line;
 
     optind = 1;
@@ -55,24 +57,24 @@ int main(int argc, char *argv[])
 
     if(buf_size == -1)
     {
-	fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
-	print_help(argv[0]);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     if(line_size == -1)
     {
-	fprintf(stderr, "%s: option requires an argument -- 'l'\n", argv[0]);
-	print_help(argv[0]);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "%s: option requires an argument -- 'l'\n", argv[0]);
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     if ((buf_size < MIN_BUF_SIZE) || (buf_size > MAX_BUF_SIZE)) 
     {
-	fprintf(stderr, "Error: El tamaño de buffer tiene que estar entre 1 y 8192.\n");
-	fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
-	print_help(argv[0]);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "Error: El tamaño de buffer tiene que estar entre 1 y 8192.\n");
+        fprintf(stderr, "%s: option requires an argument -- 'b'\n", argv[0]);
+        print_help(argv[0]);
+        exit(EXIT_FAILURE);
     }
 
 	if ((line_size < MIN_LINE_SIZE) || (line_size > MAX_LINE_SIZE)){
@@ -82,20 +84,90 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-     /* Reserva memoria dinámica para buffer de lectura */
-    if ((buffer = (char *) malloc(buf_size * sizeof(char))) == NULL)
+    //--------------------------------------------------------------//
+    ssize_t num_read;
+    ssize_t bytesTotal = 0;
+    char *buffer;
+    char *bufferComando;
+    int num_line = 1;
+    int cont = 0;
+    line_t linea;
+
+    /* Reserva memoria dinámica para buffer de lectura */
+    if ((buffer = malloc(buf_size * sizeof(char))) == NULL)
     {
         perror("malloc()");
         exit(EXIT_FAILURE);
     }
 
-    if ((bufferComando = (char *) malloc(MAX_LINE_SIZE * sizeof(char))) == NULL)
+    if ((bufferComando = malloc(line_size * sizeof(char))) == NULL)
     {
         perror("malloc()");
         exit(EXIT_FAILURE);
+    }
+
+    while ((num_read = read(STDIN_FILENO, buffer, buf_size)) > 0)
+    {
+        cont += num_read;
+        if(cont > line_size)
+        {
+            fprintf(stderr, "Error: línea %d demasiado larga: \n", num_line);
+            exit(EXIT_FAILURE);
+        }
+
+        for (ssize_t i = 0; i < num_read; i++) 
+        {
+            if (buffer[i] == CONTROL_CHAR) 
+            {
+                bufferComando[cont] = '\0';
+                linea->number = num_line;
+                linea->max_size = (line_size/2) + 1;
+                linea->command = bufferComando;
+                execute_line(linea);
+                cont = 0;
+            } 
+            else
+            {
+                bufferComando[cont] = buffer[i];
+                cont++;
+            }
+        }
+
+        num_line++;
     }
 
     printf("Size of buffer: %d\n", buf_size);
     printf("Max. size of line: %d\n", line_size);
 
+}
+
+struct line 
+{
+    int number;
+    int max_size;
+    char *command;
+};
+
+void print_help(char* nombre_programa)
+{
+	fprintf(stderr, "Uso: %s [-b BUF_SIZE] [-l MAX_LINE_SIZE]\nLee de la entrada estándar una secuencia de líneas conteniendo órdenes\npara ser ejecutadas y lanza los procesosnecesarios para ejecutar cada línea, esperando a su terminacion para ejecutar la siguiente.\n-b BUF_SIZE\tTamaño del buffer de entrada 1<=BUF_SIZE<=8192\n-l MAX_LINE_SIZE\tTamaño máximo de línea 16<=MAX_LINE_SIZE<=1024\n", nombre_programa);
+}
+
+void execute_line(line_t line)
+{
+    char **tokens = malloc(line->max_size * sizeof(char *));
+    int i = 0;
+
+    char *saveptr;
+    char *token_read = strtok_r(line->command, SEPARATOR, &saveptr);
+    while(token_read != NULL) {
+        tokens[i] = token_read;
+        i++;
+        token_read = strtok_r(NULL, SEPARATOR, &saveptr);
+    }
+
+    tokens[i] = NULL;
+
+    i = 0;
+    
 }
